ce623381bff3d9ebe1e911bbe36d72849f928a7b
AcceleratorMX <redfield242@gmail.com>
2023-05-09 11:01:46 +0300

    Update

--- src/main/java/DatabasePopulateService.java
+++ src/main/java/DatabasePopulateService.java
@@ -2,16 +2,11 @@ import tables.Client;
 import tables.Project;
 import tables.ProjectWorker;
 import tables.Worker;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+
 public class DatabasePopulateService {
     private static final String WORKER = "sqlJson/worker.json";
     private static final String CLIENT = "sqlJson/client.json";
@@ -28,19 +23,8 @@ public class DatabasePopulateService {
         databasePopulateService.projectWorkerAdd();
     }
 
-    public <T> List<T> reader(Class<T> entityClass, String filePath) {
-        List<T> jsonList = new ArrayList<>();
-        try (FileReader fileReader = new FileReader(filePath)) {
-            Type type = TypeToken.getParameterized(List.class, entityClass).getType();
-            jsonList = new Gson().fromJson(fileReader, type);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return jsonList;
-    }
-
     public void workerAdd() {
-        List<Worker> workers = reader(Worker.class, WORKER);
+        List<Worker> workers = Reader.reader(Worker.class, WORKER);
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO worker (name, birthday, level, salary) VALUES (?, ?, ?, ?)");
             workers.forEach(worker -> {
@@ -59,8 +43,9 @@ public class DatabasePopulateService {
             e.printStackTrace();
         }
     }
+
     public void clientAdd() {
-        List<Client> clients = reader(Client.class, CLIENT);
+        List<Client> clients = Reader.reader(Client.class, CLIENT);
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO client (name) VALUES (?)");
             clients.forEach(client -> {
@@ -78,7 +63,7 @@ public class DatabasePopulateService {
     }
 
     public void projectAdd() {
-        List<Project> projects = reader(Project.class, PROJECT);
+        List<Project> projects = Reader.reader(Project.class, PROJECT);
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO project (client_id, start_date, finish_date) VALUES (?, ?, ?)");
             projects.forEach(project -> {
@@ -98,7 +83,7 @@ public class DatabasePopulateService {
     }
 
     public void projectWorkerAdd() {
-        List<ProjectWorker> projectWorkers = reader(ProjectWorker.class, PROJECT_WORKER);
+        List<ProjectWorker> projectWorkers = Reader.reader(ProjectWorker.class, PROJECT_WORKER);
         try {
             preparedStatement = connection.prepareStatement("INSERT INTO project_worker (project_id , worker_id) VALUES (?, ?)");
             projectWorkers.forEach(project -> {
